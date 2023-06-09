72fc903b874dcdb8068d4262ae168842b1e1f4cf
Mohib <92154638+developerMohib@users.noreply.github.com>
2023-06-10 00:13:12 +0600

    all clear discussion

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ let sliderImages = [ 'img/mohib1.jpg' , 'img/mohib2.jpg' , 'img/mohib3.jpg' ];
 
 let image = document.querySelector('img');
 
-let stayImg = 0 ;
+let stayImg = 1 ;
 
 function next(){
+    // image 1 e bose takbe but click kelei to barbe
+
     stayImg++ ;
     let length = sliderImages.length;
     if(stayImg >= length){
+        // click kaia baira gele abar 0 index a aisa bosbe 
         stayImg = 0;
         image.src = sliderImages[stayImg];
     }
@@ -20,9 +23,11 @@ function next(){
 
 
 function previous(){
+    // akane clk kelei to kombe 
     stayImg-- ; 
+    let length = sliderImages.length;
     if(stayImg < 0){
-        let length = sliderImages.length;
+        /* click kaia kome 0 index er nice aia gele koi jabe . tkn abar length teke 1 kom a jabe */
         stayImg = length -1 ;
         image.src = sliderImages[stayImg]  ;
     }
